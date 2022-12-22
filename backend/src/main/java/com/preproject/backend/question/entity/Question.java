774a041935842cc9157ffca0d9489018db480637
jjunghakim <wlikeoxy@gmail.com>
2022-12-23 01:14:45 +0900

    feat: Question 엔티티 신규 생성 기능

--- backend/src/main/java/com/preproject/backend/question/entity/Question.java
+++ backend/src/main/java/com/preproject/backend/question/entity/Question.java
@@ -2,16 +2,44 @@ package com.preproject.backend.question.entity;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.preproject.backend.audit.Auditable;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
- 
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Setter
-public class Question {
-	private Long Id;
+@Getter
+@Entity
+public class Question extends Auditable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 255, nullable = false)
 	private String title;
+
+	@Column(length = 255, nullable = false)
 	private String content;
+
+	@Column
 	private int score;
-	private QuestionStatus status = QuestionStatus.UNRESOLVED;
+
+	@Enumerated(value = EnumType.STRING)
+	@Column(nullable = false)
+	private QuestionStatus questionStatus = QuestionStatus.UNRESOLVED;
+
 	private LocalDateTime createdAt = LocalDateTime.now();
 	private LocalDateTime modifiedAt = LocalDateTime.now();
 
