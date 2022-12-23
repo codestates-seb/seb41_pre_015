3d1c29372269f7a2b793d93284e9cdbd05872f56
choi-eun-hwa <dms6778@gmail.com>
2022-12-24 04:35:26 +0900

    docs- MainPage

--- frontend/src/component/LeftSidebar.js
+++ frontend/src/component/LeftSidebar.js
@@ -1,13 +1,9 @@
-import React from 'react'
-
 const LeftSidebar = () => {
-    return (
-        <div
-            style={{ width: '200px', backgroundColor: 'lightgoldenrodyellow' }}
-        >
-            좌측 사이드 바
-        </div>
-    )
-}
+  return (
+    <div style={{ width: '200px', backgroundColor: 'lightgoldenredyellow' }}>
+      좌측 사이드 바
+    </div>
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
