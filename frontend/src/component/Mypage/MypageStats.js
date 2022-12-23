b45c65c03d02b02729fb23375d1ec7ee2a2faa75
operat04 <operat04@naver.com>
2022-12-23 16:29:38 +0900

    docs : 문서 경로 수정

--- frontend/src/component/Mypage/MypageStats.js
+++ frontend/src/component/Mypage/MypageStats.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const MypageStats = () => {
-    return (
-        <MypagestatsContainer>
-            <div>
-                <h2>Stats</h2>
-                <div className="user_stats_box"></div>
-            </div>
-        </MypagestatsContainer>
-    )
-}
+  return (
+    <MypagestatsContainer>
+      <div>
+        <h2>Stats</h2>
+        <div className="user_stats_box"></div>
+      </div>
+    </MypagestatsContainer>
+  );
+};
 
-export default MypageStats
+export default MypageStats;
 
 const MypagestatsContainer = styled.div`
-    .user_stats_box {
-        background-color: white;
-        border: 1px solid grey;
-        border-radius: 5px;
-        width: 300px;
-        height: 150px;
-    }
-`
+  .user_stats_box {
+    background-color: white;
+    border: 1px solid grey;
+    border-radius: 5px;
+    width: 300px;
+    height: 150px;
+  }
+`;
