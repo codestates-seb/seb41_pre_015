013e42e1b1787c733a5f7203d140599518a73f92
홍동화 <hhhhong44@gmail.com>
2022-12-23 18:49:32 +0900

    feat : #52 상단바 파일 생성

---
+++ frontend/src/component/LeftSidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LeftSidebar = () => {
+    return (
+        <div
+            style={{ width: '200px', backgroundColor: 'lightgoldenrodyellow' }}
+        >
+            좌측 사이드 바
+        </div>
+    )
+}
+
+export default LeftSidebar
---
+++ frontend/src/component/RightSidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RightSidebar = () => {
+    return (
+        <div
+            style={{ width: '200px', backgroundColor: 'lightgoldenrodyellow' }}
+        >
+            우측 사이드 바
+        </div>
+    )
+}
+
+export default RightSidebar
