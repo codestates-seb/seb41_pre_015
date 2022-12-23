18479c76c79647af2664e15a9c121987d6b9f62b
wonyongg <kakaohwy@gmail.com>
2022-12-23 21:32:09 +0900

    feat: AnswerVote, QuestionVote 엔티티 구현

---
+++ backend/src/main/java/com/preproject/backend/answer/entity/AnswerVote.java
@@ -0,0 +1,40 @@
+package com.preproject.backend.answer.entity;
+
+import com.preproject.backend.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class AnswerVote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long voteId;
+
+    @Enumerated(EnumType.STRING)
+    private AnswerVoteStatus answerVoteStatus;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "answer_id")
+    private Answer answer;
+
+    public enum AnswerVoteStatus {
+        UP("추천"),
+        DOWN("비추천");
+
+        @Getter
+        private String status;
+
+        AnswerVoteStatus(String status) {
+            this.status = status;
+        }
+    }
+}
