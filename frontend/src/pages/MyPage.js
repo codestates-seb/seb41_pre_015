a4fb5341ec57c44ea9187867c07756717a48ce8a
홍동화 <hhhhong44@gmail.com>
2022-12-24 10:47:55 +0900

    feat: #53 사이즈 및 색상 추가 수정

--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 // import React from 'react'
 import styled from 'styled-components';
 
-=======
-import styled from 'styled-components';
->>>>>>> d453e27082a089ed9b5c0bfae3306b3259e698e3
 import MypageMain from '../component/Mypage/MypageMain';
 import MypageTitle from '../component/Mypage/MypageTitle';
 import MypageStats from '../component/Mypage/MypageStats';
@@ -44,11 +40,7 @@ const MypageContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-<<<<<<< HEAD
   margin: 40px 0px 0px 50px;
-=======
-  margin: 40px 0px 0px 20px;
->>>>>>> d453e27082a089ed9b5c0bfae3306b3259e698e3
 
   .MypagebottomContainer {
     margin-top: 40px;
