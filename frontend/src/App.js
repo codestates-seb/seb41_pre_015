cd522a97e0760bd7b5a68f56353ae93b5fc173f2
choi-eun-hwa <dms6778@gmail.com>
2022-12-27 00:46:42 +0900

    1. 메인페이지 퍼블리싱, 질문상세페이지 퍼블리싱 2. 라우터 기본 구조 잡기 (app.js에서 전부 관리하고 각 페이지에서 Link로 이동) 3. 백엔드 데이터 요청용 axios 모듈 설치 4. Main questions 페이지에 백엔드 /questions?page=1&size=10 호출하여 통신 연결 5. Proxy 주소 설정 (http://ec2-3-36-57-221.ap-northeast-2.compute.amazonaws.com:8080)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
 // import React from 'react';
 // import MyPage from './pages/MyPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import MainPage from './pages/MainPage';
 import MainPage from './pages/MainPage';
+import QuestionDetail from './pages/QuestionDetail';
+import Footer from './component/Footer';
+// import MainQuestions from './component/MainQuesTions';
+
+// 라우터
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <div>
-        <div>
-          <MainPage></MainPage>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/questiondetail" element={<QuestionDetail />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
