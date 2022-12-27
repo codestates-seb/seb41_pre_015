9acee27e0b7ecf695510d3a5e287b23a07fd2464
wonyongg <kakaohwy@gmail.com>
2022-12-27 17:43:50 +0900

    chore : comment 매핑

--- backend/src/main/java/com/preproject/backend/exception/ExceptionCode.java
+++ backend/src/main/java/com/preproject/backend/exception/ExceptionCode.java
@@ -8,7 +8,9 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(NOT_FOUND.value(), "Member Not Found"),
     MEMBER_ALREADY_EXISTS(CONFLICT.value(), "Member Already Exists"),
     QUESTION_NOT_FOUND(NOT_FOUND.value(), "Question Not Found"),
-    ANSWER_NOT_FOUND(NOT_FOUND.value(), "Answer Not Found");
+    ANSWER_NOT_FOUND(NOT_FOUND.value(), "Answer Not Found"),
+	ANSWER_COMMENT_NOT_FOUND(NOT_FOUND.value(), "AnswerComment Not Found"),
+	QUESTION_COMMENT_NOT_FOUND(NOT_FOUND.value(), "QuestionComment Not Found");
 
 	@Getter
 	private int status;
