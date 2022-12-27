734f8020a8c71a949f00cec63f529ca131c39242
홍동화 <hhhhong44@gmail.com>
2022-12-28 06:07:30 +0900

    feat: #53 수정페이지 기능 수정

--- frontend/src/component/Mypage/MypageEditList.js
+++ frontend/src/component/Mypage/MypageEditList.js
@@ -1,9 +1,16 @@
 // import React from 'react';
 import styled from 'styled-components';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+// import Form from 'react-bootstrap/Form';
+// eslint-disable-next-line no-unused-vars
+import { useState } from 'react';
 
-const MypageEditList = () => {
+export default function EditListMypage() {
+  const [aboutText, setAboutText] = useState('');
+  const [userName, setUserName] = useState('');
+
+  // const handleClickSubmit = () => {};
+
   return (
     <MypageEditListContainer>
       <div>
@@ -29,13 +36,41 @@ const MypageEditList = () => {
           />
           <SizesExample /> {/* 프로필 수정 버튼*/}
           <div className="edit-list">
-            <EditList />
+            <div id="inputBox">
+              <div className="lable">Display name</div>
+              <input
+                className="input"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                placeholder="이지은 (Lee ji eun)"
+              ></input>
+            </div>
+            <div id="inputBox">
+              <div className="lable">Title</div>
+              <input
+                className="input"
+                placeholder="No title has been set"
+              ></input>
+            </div>
+            <div className="editorbox">
+              <div className="lable">About</div>
+              <div className="editor">
+                <textarea
+                  value={aboutText}
+                  className="aboutInput"
+                  onChange={(e) => setAboutText(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="btnBox">
+              <button className="saveBtn">Save profile</button>
+            </div>
           </div>
         </div>
       </div>
     </MypageEditListContainer>
   );
-};
+}
 
 const HorizonLine = () => {
   return (
@@ -64,6 +99,8 @@ function SizesExample() {
           }}
           variant="secondary"
           size="lg"
+          className="saveBtn"
+          // onClick={handleClickSubmit}
         >
           Edit Picture
         </Button>
@@ -72,29 +109,29 @@ function SizesExample() {
   );
 }
 
-function EditList() {
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label style={{ fontWeight: 'bold' }}>Display name</Form.Label>
-        <Form.Control type="text" placeholder="이지은 (Lee ji eun)" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicText">
-        <Form.Label style={{ fontWeight: 'bold' }}>Title</Form.Label>
-        <Form.Control type="text" placeholder="No title has been set" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label style={{ fontWeight: 'bold' }}>About</Form.Label>
-        <Form.Control as="textarea" rows={3} style={{ width: '600px' }} />
-      </Form.Group>
-      <Button variant="warning" type="submit">
-        SAVE
-      </Button>
-    </Form>
-  );
-}
+// function EditList() {
+//   return (
+//     <Form>
+//       <Form.Group className="mb-3" controlId="formBasicEmail">
+//         <Form.Label style={{ fontWeight: 'bold' }}>Display name</Form.Label>
+//         <Form.Control type="text" placeholder="이지은 (Lee ji eun)" />
+//       </Form.Group>
+//       <Form.Group className="mb-3" controlId="formBasicText">
+//         <Form.Label style={{ fontWeight: 'bold' }}>Title</Form.Label>
+//         <Form.Control type="text" placeholder="No title has been set" />
+//       </Form.Group>
+//       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+//         <Form.Label style={{ fontWeight: 'bold' }}>About</Form.Label>
+//         <Form.Control as="textarea" rows={3} style={{ width: '600px' }} />
+//       </Form.Group>
+//       <Button variant="warning" type="submit">
+//         SAVE
+//       </Button>
+//     </Form>
+//   );
+// }
 
-export default MypageEditList;
+// export default MypageEditList;
 
 const MypageEditListContainer = styled.div`
   margin-left: 20px;
@@ -115,5 +152,81 @@ const MypageEditListContainer = styled.div`
   .edit-list {
     width: 500px;
     margin: 20px 30px 30px 30px;
+  }
+
+  .lable {
+    margin: 4px;
+    margin-right: 0;
+    margin-left: 0;
+    font-size: 1rem;
+    font-family: inherit;
+    color: black;
+    font-weight: 600;
+    padding: 0;
+  }
+
+  .input {
+    border: 1px solid rgba(111, 111, 111, 0.497);
+    padding: 10px;
+    border-radius: 3px;
+    min-width: 300px;
+    width: 40%;
+    height: 30px;
+    box-sizing: border-box;
+  }
+
+  #inputBox {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-top: 10px;
+  }
+
+  .editorbox {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-top: 20px;
+  }
+  .editor {
+    width: 100%;
+  }
+
+  .lable {
+    font-size: 1rem;
+    color: black;
+    font-weight: 600;
+    margin-bottom: 5px;
+  }
+
+  .aboutInput {
+    width: 100%;
+    height: 300px;
+    vertical-align: top;
+    padding: 10px;
+    font-size: 1rem;
+    font-weight: 600;
+    text-indent: 5px;
+    margin-bottom: 30px;
+  }
+
+  .saveBtn {
+    color: white;
+    margin-right: 0;
+    margin-left: 0;
+    margin: 1px;
+    background-color: hsl(206, 100%, 52%);
+    box-shadow: inset 0 1px 0 0 hsl(0deg 0% 100% / 40%);
+    padding: 0.8em;
+    border: 1px solid transparent;
+    border-radius: 3px;
+    outline: none;
+    font-family: inherit;
+    font-weight: normal;
+    text-align: center;
+    text-decoration: none;
+    cursor: pointer;
+    user-select: none;
   }
 `;
