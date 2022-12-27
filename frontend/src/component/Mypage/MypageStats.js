c3e3d710dbfee5c7336c20c29726a8d12c3165f6
홍동화 <hhhhong44@gmail.com>
2022-12-27 17:06:05 +0900

    feat: #53 스탯창 구현

--- frontend/src/component/Mypage/MypageStats.js
+++ frontend/src/component/Mypage/MypageStats.js
@@ -4,9 +4,31 @@ import styled from 'styled-components';
 const MypageStats = () => {
   return (
     <MypagestatsContainer>
-      <div>
-        <h2>Stats</h2>
-        <div className="user_stats_box"></div>
+      <div className="title">Stats</div>
+      <div className="StatContainer">
+        <div className="StatTop">
+          <StatDiv>
+            <StatNum>1</StatNum>
+            <StatMsg>reputation</StatMsg>
+          </StatDiv>
+
+          <StatDiv>
+            <StatNum>0</StatNum>
+            <StatMsg>reached</StatMsg>
+          </StatDiv>
+        </div>
+
+        <div className="StatBottom">
+          <StatDiv>
+            <StatNum>1</StatNum>
+            <StatMsg>answers</StatMsg>
+          </StatDiv>
+
+          <StatDiv>
+            <StatNum>0</StatNum>
+            <StatMsg>questions</StatMsg>
+          </StatDiv>
+        </div>
       </div>
     </MypagestatsContainer>
   );
@@ -14,12 +36,66 @@ const MypageStats = () => {
 
 export default MypageStats;
 
-const MypagestatsContainer = styled.div`
-  .user_stats_box {
-    background-color: white;
-    border: 1px solid gray;
-    border-radius: 5px;
-    width: 300px;
-    height: 150px;
+export const MypagestatsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  > .title {
+    font-size: 1.5em;
+    padding-bottom: 0.3em;
+    letter-spacing: -0.03em;
+  }
+
+  .StatContainer {
+    padding: 1em;
+    box-sizing: border-box;
+    gap: 1em;
+    border: 1px solid #d6d9dc;
+    border-radius: 0.3em;
+    flex-wrap: wrap;
+    @media screen and (max-width: 1050px) {
+      display: flex;
+      flex-direction: column;
+      flex-wrap: nowrap;
+    }
+  }
+
+  .StatTop {
+    display: flex;
+    flex: 1;
+    gap: 3em;
+    flex-shrink: 1;
+    @media screen and (min-width: 981px) {
+      padding-bottom: 0.5em;
+    }
+  }
+
+  .StatBottom {
+    display: flex;
+    flex: 1;
+    gap: 4em;
+    flex-shrink: 1;
+  }
+`;
+
+export const StatDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+export const StatNum = styled.p`
+  font-size: 1.2em;
+  padding-bottom: 0.2em;
+  @media screen and (max-width: 640px) {
+    font-size: 1em;
+  }
+`;
+
+export const StatMsg = styled.p`
+  font-size: 0.9em;
+  color: #626a73;
+  @media screen and (max-width: 640px) {
+    font-size: 0.8em;
   }
 `;
