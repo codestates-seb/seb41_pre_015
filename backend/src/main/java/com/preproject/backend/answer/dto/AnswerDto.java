29150bab4744460228ec3de0863666431c06cefa
wonyongg <111210881+wonyongg@users.noreply.github.com>
2022-12-30 00:21:38 +0900

    Spring RestDocs Answer 구현 (#140)
    * fix : ExceptionCode 오류 수정
    
    * refactor : acceptAnswer 관련 리펙토링
    
    * fix : question 조회 시 answercomment 매핑 안된 거 수정
    
    * feat : Spring RestDocs Answer 구현

--- backend/src/main/java/com/preproject/backend/answer/dto/AnswerDto.java
+++ backend/src/main/java/com/preproject/backend/answer/dto/AnswerDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class AnswerDto {
     @Getter
+    @AllArgsConstructor
     public static class Post {
         @NotBlank(message = "내용은 공백이 아니어야 합니다.")
         private String content;
