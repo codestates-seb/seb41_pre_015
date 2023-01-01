540302f15638c3d52cf5a0ad69a4bf94192b42f4
operat04 <operat04@naver.com>
2023-01-01 23:57:39 +0900

    feat : 답변 삭제 기능 구현

--- frontend/src/component/MainQuesTions.js
+++ frontend/src/component/MainQuesTions.js
@@ -145,7 +145,7 @@ const MainQuestions = ({ _list }) => {
                 {/* 질문작성자 정보(프로필,이름) */}
                 <QuestionImg>
                   <ProfileImg src={logo} alt="logo"></ProfileImg>
-                  <div style={{ margin: '10px' }}>User name</div>
+                  <div style={{ margin: '10px' }}>{item.memberName}</div>
                 </QuestionImg>
               </QuestionList>
               {/* 질문조회 전체영역 끝*/}
