d02ecf71777341b1101d0c8b4dd58d34b71127ee
operat04 <operat04@naver.com>
2023-01-02 19:56:20 +0900

    fix : 불필요한 코드 삭제

--- frontend/src/component/Mypage/MypageStats.js
+++ frontend/src/component/Mypage/MypageStats.js
@@ -12,19 +12,26 @@ const MypageStats = () => {
 
   useEffect(() => {
     axios
-      .get(`http://43.201.119.99:8080/members/${Number(Userdata.id)}/answers`, {
-        headers: { authorization: localStorage.getItem('accessToken') },
-      })
+      .get(
+        `http://43.201.119.99:8080/members/${Number(
+          Userdata.id
+        )}/answers?page=1&size=5`,
+        {
+          headers: { authorization: localStorage.getItem('accessToken') },
+        }
+      )
       .then((response) => {
         setAnswerView(response.data.data);
-        console.log('answer', answerView);
+        console.log('answer', response.data);
       });
   }, []);
 
   useEffect(() => {
     axios
       .get(
-        `http://43.201.119.99:8080/members/${Number(Userdata.id)}/questions`,
+        `http://43.201.119.99:8080/members/${Number(
+          Userdata.id
+        )}/questions?page=1&size=4`,
         {
           headers: { authorization: localStorage.getItem('accessToken') },
         }
